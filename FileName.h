5e0376e94e5956485461cca2d19aa78ce74eb0d3
jinggugu1110 <jinggugu1110@gmail.com>
2024-05-24 12:47:11 +0900

    Add files via upload

---
+++ FileName.h
@@ -0,0 +1,18 @@
+#pragma once
+typedef struct {
+    float X, Y;
+}CRD; //axis
+
+struct BALL {
+    CRD POS; //position
+    CRD SPEED;//speed
+    float RADIUS;//r
+    int colorR;
+    int colorG;
+    int colorB;
+
+};//ball data
+
+//BALL Ball;
+
+
